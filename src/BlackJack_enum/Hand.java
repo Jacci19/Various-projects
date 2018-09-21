653ea77b912942a100b2439b4caebf0525b1e407
Jacci19 <42783495+Jacci19@users.noreply.github.com>
2018-09-21 21:16:48 +0200

    BlackJack - working version

--- src/BlackJack_enum/Hand.java
+++ src/BlackJack_enum/Hand.java
@@ -6,6 +6,7 @@ import java.util.Collections;
 public class Hand {
 
     private ArrayList<Card> handCards;
+    private boolean win = false;
 
     public Hand() {                                     //wypełnienie talii 52 kartami
         handCards = new ArrayList<>();
@@ -13,14 +14,6 @@ public class Hand {
 
     public ArrayList<Card> getHandCards() {
         return handCards;
-    }
-
-    public void shuffle(){
-        Collections.shuffle(handCards);                     //potasowanie kart
-    }
-
-    public Card handOutCard(){
-        return handCards.remove(0);                 //zwraca pierwszą kartę z listy i jednocześnie usuwa ją z tej listy
     }
 
     public int sumHandCardsValues(){
@@ -32,8 +25,15 @@ public class Hand {
     }
 
    public void handInfo(String who) {
-        System.out.println(who.toUpperCase().charAt(0) + who.substring(1) + " hand: " + this.getHandCards());       //aby who zaczynało się wielką literą a w następnej linii małą
-        System.out.println("Sum of " + who + " hand's cards is " + this.sumHandCardsValues());
+        System.out.println("--" + who.toUpperCase().charAt(0) + who.substring(1) + " hand: " + this.getHandCards());       //aby who zaczynało się wielką literą a w następnej linii małą
+        System.out.println("--Sum of " + who + " hand's cards is: " + this.sumHandCardsValues());
    }
+
+    public boolean getWin() {
+        return win;
+    }
+    public void setWin(boolean win) {
+        this.win = win;
+    }
 
 }
--- src/BlackJack_enum/Main.java
+++ src/BlackJack_enum/Main.java
@@ -1,23 +1,19 @@
 package BlackJack_enum;
 
-import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Main {
-    Scanner inputKey;
-    String playerDecision;
-    boolean overload = false;
+    private static boolean overload = false;
+    private static boolean gameEnd = false;
     private static int overloadValue = 31;
     private static int enemyRiskLevel = 7;
 
-    private Deck croupierDeck = new Deck();
-    private Hand myHand = new Hand();
+    private static Hand myHand = new Hand();
+    private static Hand enemyHand = new Hand();
 
     public static void main(String[] args) {
 
         Deck croupierDeck = new Deck();
-        Hand myHand = new Hand();
-        Hand enemyHand = new Hand();
 
         System.out.println("Initial checkings....");
         System.out.println("CroupierDeck:  " + croupierDeck.getCards());
@@ -36,36 +32,43 @@ public class Main {
         handDeckInfo(croupierDeck, myHand, "your");
         String playerAnswer = askPlayer("Do you want next card?");            //Zwraca Y lub N
 
-        while (playerAnswer.equals("Y") || playerAnswer.equals("y")){                   //tura Gracza
+        while ((playerAnswer.equals("Y") || playerAnswer.equals("y")) && (!gameEnd)){                   //tura Gracza
             takeNextCard(croupierDeck, myHand);
-            playerAnswer = askPlayer("Do you want next card?");
+            if (!overload) playerAnswer = askPlayer("Do you want next card?");
+            else{
+                System.out.println("You overloaded!");
+                enemyHand.setWin(true);
+                gameEnd = true;
+                summary();
+            }
         }
+
         if (playerAnswer.equals("N") || playerAnswer.equals("n")){
             System.out.print("OK. Now is my turn. My first two cards are: ");           //tura Przeciwnika
             croupierDeck.giveCards(2, enemyHand, true);
             handDeckInfo(croupierDeck, enemyHand, "my");
 
-            enemyTurn(croupierDeck, enemyHand);
-            System.out.println("KONIEC");
-
+            enemyTactic(croupierDeck, enemyHand);
         }
     }
 
-    private static void enemyTurn(Deck croupierDeck, Hand enemyHand) {
+    private static void enemyTactic(Deck croupierDeck, Hand enemyHand) {
         if (overloadValue - enemyHand.sumHandCardsValues() > enemyRiskLevel){       //Jeżeli komputer akceptuje ryzyko i chce kartę...
             System.out.print("OK. I will take one more card: ");
             croupierDeck.giveCards(1, enemyHand, true);                   //...to bierze kolejną kartę
             handDeckInfo(croupierDeck, enemyHand, "my");
 
             if (enemyHand.sumHandCardsValues() >= overloadValue){                    //Jeśli jest "fura"...
-                System.out.println("Oh, I overloaded. I lose :(");                   //...to przegrywa
+                System.out.println("Oh, I overloaded... y :(");                   //...to przegrywa
+                myHand.setWin(true);
             }
             else{                                                                    //jeśli nie...
-                enemyTurn(croupierDeck, enemyHand);                                  //...to decyduje o kolejnej karcie
+                enemyTactic(croupierDeck, enemyHand);                                  //...to decyduje o kolejnej karcie
             }
         }
         else{                                                                       //jeśli nie akceptuje ryzyka...
             System.out.println("Ok, that's enough. Let's check");                   // ...to sprawdza
+            checkResult();
         }
     }
 
@@ -74,6 +77,7 @@ public class Main {
         System.out.print("Next card is: ");
         croupierDeck.giveCards(1, myHand, true);
         handDeckInfo(croupierDeck, myHand, "your");
+        if (myHand.sumHandCardsValues() >= overloadValue) overload = true;
     }
 
 
@@ -89,10 +93,25 @@ public class Main {
     }
 
     public static void handDeckInfo(Deck deck, Hand hand, String who) {
-        System.out.println("Deck's size:  " + deck.getSize());
+        //System.out.println("Deck's size:  " + deck.getSize());
         hand.handInfo(who);
     }
 
+    static void checkResult(){
+        System.out.println("Your sum is: " + myHand.sumHandCardsValues() + ", my sum is: " + enemyHand.sumHandCardsValues());
+        if (myHand.sumHandCardsValues() > enemyHand.sumHandCardsValues()){
+            myHand.setWin(true);
+        }
+        else {
+           enemyHand.setWin(true);
+        }
+        summary();
+    }
 
+    static void summary(){
+        if (myHand.getWin() == true) System.out.println("YOU WIN!");
+        if (enemyHand.getWin() == true) System.out.println("YOU LOSE!");
+        System.out.println("---GAME OVER---");
+    }
 
 }
