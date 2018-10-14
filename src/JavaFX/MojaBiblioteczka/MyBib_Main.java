5927781b6c8a0b791f49e396532aceb802726668
Jacek <jacek205@wp.pl>
2018-10-14 20:24:47 +0200

    Moja Biblioteczka - Resource Bundles - (multilanguage)

--- src/JavaFX/MojaBiblioteczka/MyBib_Main.java
+++ src/JavaFX/MojaBiblioteczka/MyBib_Main.java
@@ -8,6 +8,9 @@ import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
+import java.util.Locale;
+import java.util.ResourceBundle;
+
 public class MyBib_Main extends Application {
 
     public static void main(String[] args) {
@@ -16,10 +19,15 @@ public class MyBib_Main extends Application {
 
     public void start(Stage primaryStage) throws Exception{
         FXMLLoader loader = new FXMLLoader(this.getClass().getResource("FXML/BorderPaneMain.fxml"));
+
+        Locale.setDefault(new Locale("en"));                                                            //jak zmienimy na "en" to będą przyciski po angielsku
+        ResourceBundle bundle = ResourceBundle.getBundle("JavaFX.MojaBiblioteczka.Bundles.messages");
+        loader.setResources(bundle);
+
         BorderPane myBorderPane = loader.load();
         Scene myScene = new Scene(myBorderPane);
         primaryStage.setScene(myScene);
-        primaryStage.setTitle("Moja biblioteczka");
+        primaryStage.setTitle(bundle.getString("title.application"));
         primaryStage.show();
     }
 
