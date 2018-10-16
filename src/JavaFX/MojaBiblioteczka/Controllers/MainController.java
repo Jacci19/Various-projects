255c57ca422e37c74ae1ed7258a6dc6ab2f4b49a
Jacci19 <jacek205@wp.pl>
2018-10-16 15:50:39 +0200

    Moja biblioteczka - Lesson 27 - button and loading forms

--- src/JavaFX/MojaBiblioteczka/Controllers/MainController.java
+++ src/JavaFX/MojaBiblioteczka/Controllers/MainController.java
@@ -1,7 +1,12 @@
 package JavaFX.MojaBiblioteczka.Controllers;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.layout.BorderPane;
+
+import java.io.IOException;
+import java.util.ResourceBundle;
 
 public class MainController {
 
@@ -14,5 +19,19 @@ public class MainController {
     @FXML
     private void initialize(){
         topMenuButtonsController.setMainController(this);
+    }
+
+    public void setCenter(String fxmlPath){
+        FXMLLoader loader = new FXMLLoader(this.getClass().getResource(fxmlPath));                //skopiowane z Main (3 linie + czwarta przerobiona na parent
+        ResourceBundle bundle = ResourceBundle.getBundle("JavaFX.MojaBiblioteczka.Bundles.messages");
+        loader.setResources(bundle);
+        Parent parent = null;
+        try {
+            parent = loader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        borderPane.setCenter(parent);
     }
 }
