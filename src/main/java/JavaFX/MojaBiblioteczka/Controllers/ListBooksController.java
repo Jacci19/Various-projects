12c02aea2580a40265283c854bfe2f353d385a8f
Jacek <jacek205@wp.pl>
2018-11-04 11:19:47 +0100

    Moja biblioteczka - lesson 46 (add ListBooks form)

---
+++ src/main/java/JavaFX/MojaBiblioteczka/Controllers/ListBooksController.java
@@ -0,0 +1,12 @@
+package JavaFX.MojaBiblioteczka.Controllers;
+
+import javafx.fxml.FXML;
+
+public class ListBooksController {
+
+    @FXML
+    void initialize(){
+        System.out.println("hello");
+
+    }
+}
