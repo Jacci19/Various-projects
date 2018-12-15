4ad9abc428e996257514ab66e70e36b72d41dd8c
Jacci19 <jacek205@wp.pl>
2018-12-15 12:46:13 +0100

    SpaceRunner L_11 (gameViewManager - background moving)

--- src/main/java/JavaFX/SpaceRunner/View/GameViewManager.java
+++ src/main/java/JavaFX/SpaceRunner/View/GameViewManager.java
@@ -8,6 +8,7 @@ import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 public class GameViewManager {
@@ -25,6 +26,10 @@ public class GameViewManager {
     private boolean isRightKeyPressed;
     private int angle;
     private AnimationTimer gameTimer;
+
+    private GridPane gridPane1;                                 //umieścimy w nim tło gry
+    private GridPane gridPane2;
+    private static final String BACKGROUND_IMAGE = "JavaFX/SpaceRunner/blueBackground.png";
 
     public GameViewManager() {                                  //konstruktor
         initializeStage();
@@ -64,6 +69,7 @@ public class GameViewManager {
     public void createNewGame(Stage menuStage, SHIP chosenShip){                    //metoda wywoływana z klasy ViewManager po naciśnięciu START
         this.menuStage = menuStage;
         this.menuStage.hide();
+        createBackground();
         createShip(chosenShip);
         createGameLoop();
         gameStage.show();
@@ -80,6 +86,7 @@ public class GameViewManager {
         gameTimer = new AnimationTimer() {
             @Override
             public void handle(long now) {
+                moveBackGround();
                 moveShip();
             }
         };
@@ -122,5 +129,34 @@ public class GameViewManager {
             ship.setRotate(angle);
         }
 
+    }
+
+    private void createBackground(){
+        gridPane1 = new GridPane();
+        gridPane2 = new GridPane();
+
+        for (int i=0; i < 12; i++){                                                         //ten gridPane ma 12 pól
+            ImageView backgroundImage1 = new ImageView(BACKGROUND_IMAGE);
+            ImageView backgroundImage2 = new ImageView(BACKGROUND_IMAGE);
+            GridPane.setConstraints(backgroundImage1, i%3, i/3);        //ustawienie ograniczeń
+            GridPane.setConstraints(backgroundImage2, i%3, i/3);
+            gridPane1.getChildren().add(backgroundImage1);
+            gridPane2.getChildren().add(backgroundImage2);
+
+        }
+        gridPane2.setLayoutY(-1024);
+        gamePane.getChildren().addAll(gridPane1, gridPane2);
+    }
+
+    private void moveBackGround(){
+        gridPane1.setLayoutY(gridPane1.getLayoutY() + 0.5);                             //tło przesuwa się w każdej klatce timera
+        gridPane2.setLayoutY(gridPane2.getLayoutY() + 0.5);
+
+        if (gridPane1.getLayoutY() >= 1024){                                            //gdy tło dołem wyjdzie poza stage to ustawia się na górze
+            gridPane1.setLayoutY(-1024);
+        }
+        if (gridPane2.getLayoutY() >= 1024){
+            gridPane2.setLayoutY(-1024);
+        }
     }
 }
